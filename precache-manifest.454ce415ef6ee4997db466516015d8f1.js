52edb48d67d1420e161a423da86bef9248577477
erasedbeyond <shivendra22031996@gmail.com>
2020-08-27 19:19:24 +0530

    Updates

---
+++ precache-manifest.454ce415ef6ee4997db466516015d8f1.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d2d85a56494d36fd7e1ad9ebd0c9f4",
+    "url": "/my-ipod/index.html"
+  },
+  {
+    "revision": "cb36c3b9417596cbdf28",
+    "url": "/my-ipod/static/css/main.58e5f13a.chunk.css"
+  },
+  {
+    "revision": "3b104ab449ea09defe89",
+    "url": "/my-ipod/static/js/2.299a9812.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/my-ipod/static/js/2.299a9812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb36c3b9417596cbdf28",
+    "url": "/my-ipod/static/js/main.2611989d.chunk.js"
+  },
+  {
+    "revision": "e1fb83059c1c2a8f3814",
+    "url": "/my-ipod/static/js/runtime-main.033c3ad1.js"
+  },
+  {
+    "revision": "95145a3190f720268da9ccea14767021",
+    "url": "/my-ipod/static/media/album.95145a31.svg"
+  },
+  {
+    "revision": "02fde801d2d8617c368f69bef46a974b",
+    "url": "/my-ipod/static/media/albumIcon.02fde801.svg"
+  },
+  {
+    "revision": "4884129dfbc5d947669420b80201bf6d",
+    "url": "/my-ipod/static/media/allSongsIcon.4884129d.svg"
+  },
+  {
+    "revision": "b131000588ff00f79f067bb869ab8bda",
+    "url": "/my-ipod/static/media/artist.b1310005.svg"
+  },
+  {
+    "revision": "eb59514b1bc8d5592796d8d70ce9e0d4",
+    "url": "/my-ipod/static/media/artistIcon.eb59514b.svg"
+  },
+  {
+    "revision": "f0e96c88a10e1aed4496274b83e79995",
+    "url": "/my-ipod/static/media/batteryIcon.f0e96c88.svg"
+  },
+  {
+    "revision": "6e9bdf04a5114cff3f591e036cb105de",
+    "url": "/my-ipod/static/media/click.6e9bdf04.mp3"
+  },
+  {
+    "revision": "93c6e8afaf15b15f1f4f4a275b1c52af",
+    "url": "/my-ipod/static/media/colorIcon.93c6e8af.svg"
+  },
+  {
+    "revision": "48bbaf1a2133173b6f7c76889f1c9b9a",
+    "url": "/my-ipod/static/media/fontIcon.48bbaf1a.svg"
+  },
+  {
+    "revision": "c07788236b70b82d91bf2a4c3bfa1179",
+    "url": "/my-ipod/static/media/gameIcon.c0778823.svg"
+  },
+  {
+    "revision": "1f1751be67f08279fc0ed6c6f5b894a1",
+    "url": "/my-ipod/static/media/loopIcon.1f1751be.svg"
+  },
+  {
+    "revision": "0fbd910cb05f2d5052f69c19ce0312de",
+    "url": "/my-ipod/static/media/nowPlayingIcon.0fbd910c.svg"
+  },
+  {
+    "revision": "b305c9666ac444b67bab28a0b0f41a42",
+    "url": "/my-ipod/static/media/off.b305c966.svg"
+  },
+  {
+    "revision": "bc4fb86abc2c8c3b7a9e1397549854ab",
+    "url": "/my-ipod/static/media/onn.bc4fb86a.svg"
+  },
+  {
+    "revision": "72ef6b3124e88333b806c651a5f6717b",
+    "url": "/my-ipod/static/media/pauseIcon.72ef6b31.svg"
+  },
+  {
+    "revision": "23702280789a59631be14a16f188261b",
+    "url": "/my-ipod/static/media/play.23702280.svg"
+  },
+  {
+    "revision": "b1a38254b91ff9d9c8a2ad0cf73698c9",
+    "url": "/my-ipod/static/media/playIcon.b1a38254.svg"
+  },
+  {
+    "revision": "1d13f8c23daa5d0eaefb122039dff4ce",
+    "url": "/my-ipod/static/media/repeatIcon.1d13f8c2.svg"
+  },
+  {
+    "revision": "40b8ac78c25a53336fd16c5dbeccfe50",
+    "url": "/my-ipod/static/media/settingIcon.40b8ac78.svg"
+  },
+  {
+    "revision": "43371a6f3a151e9a89a33271d3345154",
+    "url": "/my-ipod/static/media/song1.43371a6f.mp3"
+  },
+  {
+    "revision": "e3b6e2eae628414f1ddf870f59947a68",
+    "url": "/my-ipod/static/media/song1.e3b6e2ea.jpg"
+  },
+  {
+    "revision": "8df11fa203bff18a3b23f3d6b0c5eb3c",
+    "url": "/my-ipod/static/media/song10.8df11fa2.mp3"
+  },
+  {
+    "revision": "eba2c612183cf0ae6b7dc2cc671fa5f6",
+    "url": "/my-ipod/static/media/song11.eba2c612.mp3"
+  },
+  {
+    "revision": "309294a827a13c8eab4ca6e6b7164e33",
+    "url": "/my-ipod/static/media/song12.309294a8.jpg"
+  },
+  {
+    "revision": "b79a8a09b8929a960119f4df8cc188d5",
+    "url": "/my-ipod/static/media/song12.b79a8a09.mp3"
+  },
+  {
+    "revision": "f75795c9e9f34e1a2ffeeddf495c1c36",
+    "url": "/my-ipod/static/media/song2.f75795c9.mp3"
+  },
+  {
+    "revision": "efeab5fd9caaeb713c11dca760be88d7",
+    "url": "/my-ipod/static/media/song3.efeab5fd.mp3"
+  },
+  {
+    "revision": "823d03556f3a9edb2ad0436a02035ce5",
+    "url": "/my-ipod/static/media/song4.823d0355.mp3"
+  },
+  {
+    "revision": "670092f98e45611fb21edd864bb63387",
+    "url": "/my-ipod/static/media/song5.670092f9.mp3"
+  },
+  {
+    "revision": "8219b17aee0c4d89d1e255b8252c2093",
+    "url": "/my-ipod/static/media/song5.8219b17a.jpg"
+  },
+  {
+    "revision": "89fb7dd7993f7fd9c3e7275d35e2b68a",
+    "url": "/my-ipod/static/media/song6.89fb7dd7.mp3"
+  },
+  {
+    "revision": "7a0607ef2d0d7d7ff25c18d92a49ddfa",
+    "url": "/my-ipod/static/media/song7.7a0607ef.mp3"
+  },
+  {
+    "revision": "d316f3cac492bfacf5bac49257765879",
+    "url": "/my-ipod/static/media/song7.d316f3ca.jpg"
+  },
+  {
+    "revision": "e2281440c7cedd9d80af68b5b336e6e4",
+    "url": "/my-ipod/static/media/song8.e2281440.mp3"
+  },
+  {
+    "revision": "bdeabe2ddba9fe019c000104b516e5d2",
+    "url": "/my-ipod/static/media/song9.bdeabe2d.mp3"
+  },
+  {
+    "revision": "2c7719d2a925eb026eb32beb36ae386c",
+    "url": "/my-ipod/static/media/songIcon.2c7719d2.svg"
+  },
+  {
+    "revision": "8c6d735b2516ba1af4e0db0bbfb988aa",
+    "url": "/my-ipod/static/media/songSettingIcon.8c6d735b.svg"
+  },
+  {
+    "revision": "556e6d8cce94e332bdb26d6e7dcc1cef",
+    "url": "/my-ipod/static/media/suffleIcon.556e6d8c.svg"
+  },
+  {
+    "revision": "8ad050dc84d636dc36a6c2dad66cf591",
+    "url": "/my-ipod/static/media/themeIcon.8ad050dc.svg"
+  },
+  {
+    "revision": "9ab77b0c890522d9d4a7fdf146b2c632",
+    "url": "/my-ipod/static/media/unlock.9ab77b0c.mp3"
+  },
+  {
+    "revision": "f54cafb82e544b6a10a2396b4511239c",
+    "url": "/my-ipod/static/media/video1.f54cafb8.mp4"
+  },
+  {
+    "revision": "7ec27189126361c11c0e1bd9e92e155e",
+    "url": "/my-ipod/static/media/video2.7ec27189.mp4"
+  },
+  {
+    "revision": "bbd82d1e187dae694f41830fbf9e26b2",
+    "url": "/my-ipod/static/media/video3.bbd82d1e.mp4"
+  },
+  {
+    "revision": "df4e1575c267fcbda01ea661878866f6",
+    "url": "/my-ipod/static/media/video4.df4e1575.mp4"
+  },
+  {
+    "revision": "fc7a414465eb08f49db20d0c75cfcaf9",
+    "url": "/my-ipod/static/media/video5.fc7a4144.mp4"
+  },
+  {
+    "revision": "49ca50bbfb7d2baf2a2680eeb7ed5b48",
+    "url": "/my-ipod/static/media/video6.49ca50bb.mp4"
+  },
+  {
+    "revision": "9fbcb9df528845470ce593850670373b",
+    "url": "/my-ipod/static/media/video7.9fbcb9df.mp4"
+  },
+  {
+    "revision": "6f07f981d2cc0eb108e1f92ad502471a",
+    "url": "/my-ipod/static/media/videoIcon.6f07f981.svg"
+  },
+  {
+    "revision": "f54416c964468153f5b7ae91e3b0533a",
+    "url": "/my-ipod/static/media/wallpaper1.f54416c9.jpg"
+  },
+  {
+    "revision": "ef432ee72e1b081185e5684296c838e6",
+    "url": "/my-ipod/static/media/wallpaper2.ef432ee7.jpg"
+  },
+  {
+    "revision": "60df14fe70773f79ff078a24fa70a500",
+    "url": "/my-ipod/static/media/wallpaperIcon.60df14fe.svg"
+  }
+]);
--- src/assets/datafiles/WallpaperData.js
+++
@@ -1,8 +0,0 @@
-import wallpaper1 from '../image/wallpaper/wallpaper1.jpg'
-import wallpaper2 from '../image/wallpaper/wallpaper2.jpg'
-import wallpaper3 from '../image/wallpaper/wallpaper3.jpg'
-
-export const wallpaper = [
-    wallpaper1,wallpaper2,wallpaper3
-]
-
